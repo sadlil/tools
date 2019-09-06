ccd3aa6ece80aeb3958cee9d85cd4fb9349bde4d
Sadlil Rhythom <sadlil.rhythm@gmail.com>
2019-09-06 16:18:57 +0200

    Add multi kube command to run same kubectl commands to a list of clusters

---
+++ cmd/multikubectl/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/sadlil/tools/pkg/multikubectl/cmd"
+	"k8s.io/kubectl/pkg/util/logs"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	command := cmd.NewDefaultMultiKubectlCommand()
+
+	// cliflag.InitFlags()
+	logs.InitLogs()
+	defer logs.FlushLogs()
+
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/sadlil/tools
+
+go 1.13
+
+require (
+	github.com/spf13/cobra v0.0.5
+	k8s.io/client-go v0.0.0-20190831074946-3fe2abece89e
+	k8s.io/klog v0.4.0
+	k8s.io/kubectl v0.0.0-20190831163037-3b58a944563f
+)
